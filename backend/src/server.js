df6147010aee8da7df0b358a189c207e3ea11b37
ToanTran <ttt01092005@gmail.com>
2025-10-24 12:23:10 +0700

    ooooooo

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,17 +14,17 @@ const app = express();
 //middlewares
 
 app.use(express.json());
-if (process.env.NODE_END !== "production") {
+if (process.env.NODE_ENV !== "production") {
   app.use(cors({ origin: "http://localhost:5173" }));
 }
 
 app.use("/api/tasks", taskRoute);
 
-if (process.env.NODE_END === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
